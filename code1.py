e90f54ea4a934c6402a82610a2a7409ecc2892cd
BuildTools <unconfigured@null.spigotmc.org>
2024-09-22 23:50:48 +0100

    weve got snowballs chance in hell actualy added the main code with modularity

---
+++ code1.py
@@ -0,0 +1,76 @@
+from PIL import Image as img
+import math as m
+import time
+
+
+class pixelsort:
+    def __init__(self,settings = [1,1,1,10,-1]):
+        self.rconstant = settings[0]
+        self.gconstant = settings[1]
+        self.bconstant = settings[2]
+        self.heightconstant = settings[3]
+        self.flip = settings[4]
+    
+
+    def evalu(self,val):     #this determins how the sort works
+        return ((val[0]*self.rconstant+val[1]*self.gconstant+val[2]*self.bconstant)*self.flip + val[3]*self.heightconstant)
+    
+    
+    def bloodygo(self):
+        while True:
+            path = input("input a filepath, and get it right")
+            try:
+                the_image = img.open(path)
+                break
+            except:
+                pass
+
+        
+        time1 = time.time()
+        pixel_map = the_image.load()
+        width, height = the_image.size
+
+        frac_hgt = height
+        fractions = height//frac_hgt
+        remainda = height%fractions
+        lastpercent = 0
+
+        for i in range(width):#across
+            lizt = []
+            offset = 0                                                 #should be toggleable
+            frac_hgt = height                                          #round(height *  ((m.sin(i*2)/2.05) + 0.5))
+            fractions = height//frac_hgt
+            remainda = height%fractions
+
+            for t in range(fractions):
+                lizt = []
+                for j in range(frac_hgt):
+                    lizt.append((pixel_map[i,j+offset][0], pixel_map[i,j+offset][1] ,pixel_map[i,j+offset][2], j)) 
+
+                lizt.sort(key = self.evalu)
+                    
+                for j in range(frac_hgt): 
+                    pixel_map[i,j+offset] = lizt[j]    
+
+                offset = offset + frac_hgt
+            
+            for j in range(remainda):
+                lizt.append((pixel_map[i,j+offset][0], pixel_map[i,j+offset][1] ,pixel_map[i,j+offset][2], j))    
+                lizt.sort(key = self.evalu)
+
+            for j in range(remainda):
+                pixel_map[i,j+offset] = lizt[j]        
+
+            if (round(i/width,2) > lastpercent):
+                lastpercent = round(i/width,2)
+                print(str(lastpercent*100))
+        
+        time2 = time.time()
+        runtime = time2 - time1
+        print(runtime)
+        the_image.show()
+        print("'done killing'")
+        the_image.save("sorted/"+str(time.time()).replace(".","")+".png")
+
+lets = pixelsort()
+lets.bloodygo()
